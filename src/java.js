3a4b6320f3c6e1aab88c9ed739e00b72e00a71e1
DARAZOUKI <Aussama@chalmers.se>
2024-02-26 17:01:57 +0100

    new edit

--- src/java.js
+++ src/java.js
@@ -1,5 +1,6 @@
-"use strict";
-import Chart from 'chart.js/auto';
+// Import Chartist.js
+import * as Chartist from 'chartist';
+
 
 document.addEventListener("DOMContentLoaded", function () {
     fetchDataAndGenerateCharts();
@@ -24,66 +25,26 @@ async function fetchDataAndGenerateCharts() {
     }
 }
 
-function getTopCourses(data, count) {
-    return data.filter(item => item.type === 'Kurs')
-        .sort((a, b) => b.applicantsTotal - a.applicantsTotal)
-        .slice(0, count);
-}
-
-function getTopPrograms(data, count) {
-    return data.filter(item => item.type === 'Program')
-        .sort((a, b) => b.applicantsTotal - a.applicantsTotal)
-        .slice(0, count);
-}
-
 function createBarChart(coursesData) {
-    const ctx = document.getElementById('barChart').getContext('2d');
-    new Chart(ctx, {
-        type: 'bar',
-        data: {
-            labels: coursesData.map(course => course.name),
-            datasets: [{
-                label: 'Total Sökande',
-                data: coursesData.map(course => parseInt(course.applicantsTotal)),
-                backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                borderColor: 'rgba(75, 192, 192, 1)',
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true
-                }
-            }
+    const labels = coursesData.map(course => course.name);
+    const series = [coursesData.map(course => parseInt(course.applicantsTotal))];
+
+    new Chartist.Bar('.ct-bar-chart', {
+        labels: labels,
+        series: series
+    }, {
+        axisY: {
+            onlyInteger: true
         }
     });
 }
 
 function createPieChart(programsData) {
-    const ctx = document.getElementById('pieChart').getContext('2d');
-    new Chart(ctx, {
-        type: 'pie',
-        data: {
-            labels: programsData.map(program => program.name),
-            datasets: [{
-                data: programsData.map(program => parseInt(program.applicantsTotal)),
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                ],
-                borderWidth: 1
-            }]
-        }
+    const labels = programsData.map(program => program.name);
+    const series = programsData.map(program => parseInt(program.applicantsTotal));
+
+    new Chartist.Pie('.ct-pie-chart', {
+        labels: labels,
+        series: series
     });
 }
